a9d4b14458ae5411391e9df2505ab1b8d8a01a7b
German <thewhitehaven04@gmail.com>
2025-02-20 22:57:25 +0100

    Update previews

--- app/projects/projectData.tsx
+++ app/projects/projectData.tsx
@@ -1,9 +1,10 @@
 import type { ProjectCard } from "app/projects/ProjectCard"
 import type { ComponentProps } from "react"
-import f1StatsImg1 from "public/F1StatsProject/img1.png"
 import f1StatsImg2 from "public/F1StatsProject/img2.png"
 import f1StatsImg3 from "public/F1StatsProject/img3.png"
 import f1StatsImg4 from "public/F1StatsProject/img4.png"
+import f1StatsImg5 from "public/F1StatsProject/img5.png"
+import f1StatsImg6 from "public/F1StatsProject/img6.png"
 import { SiChartdotjs, SiDaisyui, SiFastapi, SiPandas, SiReact, SiReactrouter, SiTailwindcss } from "react-icons/si"
 import { CaptionedIcon } from "app/components/CaptionedIcon"
 
@@ -18,10 +19,11 @@ export const projects: ComponentProps<typeof ProjectCard>[] = [
         url: "https://github.com/thewhitehaven04/f1stats",
         demoUrl: "https://f1stats-production.up.railway.app/",
         images: [
-            { ...f1StatsImg1, id: "f1stats-img1", alt: "F1Stats screenshot 1" },
-            { ...f1StatsImg2, id: "f1stats-img2", alt: "F1Stats screenshot 2" },
-            { ...f1StatsImg3, id: "f1stats-img3", alt: "F1Stats screenshot 3" },
-            { ...f1StatsImg4, id: "f1stats-img4", alt: "F1Stats screenshot 4" },
+            { ...f1StatsImg2, id: "f1stats-img1", alt: "F1Stats screenshot 1" },
+            { ...f1StatsImg3, id: "f1stats-img2", alt: "F1Stats screenshot 2" },
+            { ...f1StatsImg4, id: "f1stats-img3", alt: "F1Stats screenshot 3" },
+            { ...f1StatsImg5, id: "f1stats-img4", alt: "F1Stats screenshot 4" },
+            { ...f1StatsImg6, id: "f1stats-img5", alt: "F1Stats screenshot 5" },
         ],
         stack: (
             <>
