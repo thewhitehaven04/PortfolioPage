d42db73236dc32bed623d195dd37ed4d1738afb3
German <thewhitehaven04@gmail.com>
2025-02-22 17:55:49 +0100

    Metadata updates

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -5,6 +5,7 @@ import type { ComponentProps } from "react"
 import Image from "next/image"
 import Profile from "public/profile.png"
 import { Contacts } from "app/components/Header/Contacts"
+import { Summary } from "app/components/Header/Summary"
 
 const aboutMe: ComponentProps<typeof AboutMe> = {
     name: "German Bulavkin",
@@ -15,18 +16,7 @@ const aboutMe: ComponentProps<typeof AboutMe> = {
 export const Header = () => (
     <header className="flex flex-col gap-4">
         <AboutMe {...aboutMe} />
-        <section className="flex flex-col pl-[80px]">
-            <div className='grid grid-cols-[1px,64px,_1fr]'>
-                <div className='h-8 border-2 border-primary rounded-sm'/>
-                <div className='border-b-4 border-l-4 border-primary w-16 h-8 skew-x-[45deg] translate-x-4 rounded-b-sm'/>
-                <div className="border-b-4 w-full border-primary" />
-            </div>
-            <p className="block font-xl border-4 border-t-0 rounded-b-xl p-4 border-primary">
-                Front-end engineer with a vast background in quality assurance, passionate about all things tech and
-                computer hardware, with a lot of experience working with dynamic international teams. I enjoy bringing
-                user interfaces to life, helping build better products. Let's connect!
-            </p>
-        </section>
+        <Summary />
         <Contacts />
         <Navbar />
     </header>
--- app/og/route.tsx
+++ app/og/route.tsx
@@ -1,19 +1,17 @@
 import { ImageResponse } from "next/og"
-import { metaData } from "app/config"
+import Profile from "public/profile.png"
+import Image from "next/image"
 
 export function GET(request: Request) {
-    let url = new URL(request.url)
-    let title = url.searchParams.get("title") || metaData.title
-
     return new ImageResponse(
-        <div tw="flex flex-col w-full h-full items-center justify-center bg-white">
-            <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
-                <h2 tw="flex flex-col text-4xl font-bold tracking-tight text-left">{title}</h2>
+        <div className="avatar col-start-1 p-3 row-start-1 row-span-3 items-center justify-center">
+            <div className="ring-4 ring-primary ring-offset-8 ring-offset-base-100 rounded-full">
+                <Image src={Profile} alt="Profile" height={512} width={512} />
             </div>
         </div>,
         {
-            width: 1200,
-            height: 630,
+            width: 512,
+            height: 512,
         },
     )
 }
